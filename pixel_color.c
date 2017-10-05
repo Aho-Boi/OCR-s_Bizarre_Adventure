8d8efc45bf24ca1bc55c565a7d2f48d7c737cc9d
Ugo Chane You Kaye <ugochane@gmail.com>
2017-10-05 15:25:45 +0200

    Presque correcte pb de complil

--- pixel_color.c
+++ pixel_color.c
@@ -1,4 +1,63 @@
-# include <pixel_color.h>
+# include "pixel_color.h"
+
+void init_sdl(void) {
+  // Init only the video part
+  if( SDL_Init(SDL_INIT_VIDEO)==-1 ) {
+    // If it fails, die with an error message
+    errx(1,"Could not initialize SDL: %s.\n", SDL_GetError());
+  }
+  // We don't really need a function for that ...
+}
+
+void wait_for_keypressed(void) {
+  SDL_Event             event;
+  // Infinite loop, waiting for event
+  for (;;) {
+    // Take an event
+    SDL_PollEvent( &event );
+    // Switch on event type
+    switch (event.type) {
+    // Someone pressed a key -> leave the function
+    case SDL_KEYDOWN: return;
+    default: break;
+    }
+  // Loop until we got the expected event
+  }
+}
+
+SDL_Surface* load_image(char *path) {
+  SDL_Surface          *img;
+  // Load an image using SDL_image with format detection
+  img = IMG_Load(path);
+  if (!img)
+    // If it fails, die with an error message
+    errx(3, "can't load %s: %s", path, IMG_GetError());
+  return img;
+}
+
+SDL_Surface* display_image(SDL_Surface *img) {
+  SDL_Surface          *screen;
+  // Set the window to the same size as the image
+  screen = SDL_SetVideoMode(img->w, img->h, 0, SDL_SWSURFACE|SDL_ANYFORMAT);
+  if ( screen == NULL ) {
+    // error management
+    errx(1, "Couldn't set %dx%d video mode: %s\n",
+         img->w, img->h, SDL_GetError());
+  }
+ 
+  /* Blit onto the screen surface */
+  if(SDL_BlitSurface(img, NULL, screen, NULL) < 0)
+    warnx("BlitSurface error: %s\n", SDL_GetError());
+ 
+  // Update the screen
+  SDL_UpdateRect(screen, 0, 0, img->w, img->h);
+ 
+  // wait for a key
+  wait_for_keypressed();
+ 
+  // return the screen for further uses
+  return screen;
+}
 
 static inline
 Uint8* pixelref(SDL_Surface *surf, unsigned x, unsigned y)
@@ -55,7 +114,7 @@ void putpixel(SDL_Surface *surface, unsigned x, unsigned y, Uint32 pixel)
 
 void Set_pixel_gray(SDL_Surface *surface)
 {
- for(int x = 0; x < *surface->w; x++)
+ for(int x = 0; x < surface->w; x++)
  {
   for(int y = 0; y < surface->h; y++)
   {
@@ -63,33 +122,47 @@ void Set_pixel_gray(SDL_Surface *surface)
    Uint8 r;
    Uint8 g;
    Uint8 b;
-   SDL_GetRGB(pixel, *surface->format, &r, &g, &b);
+   SDL_GetRGB(pixel, surface->format, &r, &g, &b);
    Uint8 gray_pixel = 0.299 * r + 0.587 * g + 0.114 * b;
-   Uint32 gray = SDL_MapRGB(*surface, gray_pixel, gray_pixel, gray_pixel);
-   putpixel(*surface, x, y, gray);
+   Uint32 gray = SDL_MapRGB(surface->format, gray_pixel, gray_pixel, gray_pixel);
+   putpixel(surface, x, y, gray);
   }
  }
 }
 
 void Set_pixel_black_white(SDL_Surface *surface)
 {
- for(int x = 0; x < *surface->w; x++)
+ Uint32 black;
+ Uint32 white;
+ for(int x = 0; x < surface->w; x++)
  {
-  for(int y = 0; y < *surface->h; y++)
+  for(int y = 0; y < surface->h; y++)
   {
-   Uint32 pixel = getpixel(*surface, x, y);
+   Uint32 pixel = getpixel(surface, x, y);
    Uint8 r;
    Uint8 g;
    Uint8 b;
-   SDL_GetRGB(pixel, *surface->format, &r, &g, &b);
+   SDL_GetRGB(pixel, surface->format, &r, &g, &b);
    if(r > 128)
    {
-        Uint32 white = SDL_MapRGB(*surface, 255, 255, 255);
-        putpixel(*surface, x, y, white);
+        white = SDL_MapRGB(surface->format, 255, 255, 255);
+        putpixel(surface, x, y, white);
    }
    else
-        Uint32 black = SDL_MapRGB(*surface, 0,0, 0);
-	putpixel(*surface, x, y, black);
+   {
+        black = SDL_MapRGB(surface->format, 0,0, 0);
+		putpixel(surface, x, y, black);
+	}
   }	
  }
 }
+
+int main(int argc, char *argv[])
+{
+	init_sdl();
+	SDL_Surface* image = load_image(argv[1]);
+	Set_pixel_black_white(image);
+	SDL_Surface* disp = display_image(image);
+}
+
+
--- pixel_color.h
+++ pixel_color.h
@@ -2,7 +2,12 @@
 # define PIXEL_COLOR_H_
 
 # include <stdlib.h>
-# include <SDL.h>
+# include <stdio.h>
+# include <SDL/SDL.h>
+# include <SDL/SDL_image.h>
+# include <err.h>
 
 Uint32 getpixel(SDL_Surface *surface, unsigned x, unsigned y);
 void putpixel(SDL_Surface *surface, unsigned x, unsigned y, Uint32 pixel);
+
+# endif
