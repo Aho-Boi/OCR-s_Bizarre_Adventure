f2b67067e5b6b26a4ea0a77a8b8e68ffdf4ccfda
Antoine Hacquard <antoine.hacquard@epita.fr>
2017-10-13 00:39:17 +0200

    chepa ce que je commit mais je suis pas a jour

---
+++ xor_func.h
@@ -0,0 +1,26 @@
+#ifndef _XOR_FUNC_
+#define _XOR_FUNC_
+
+typedef struct neuron neuron;
+
+struct neuron
+{
+  float weight[2];
+  int size;
+  float error;
+  float exit;
+};
+
+int strlenP(const char *str);
+float activation(float value);
+void work (int *input, int lines, neuron *hidden, neuron output);
+neuron* neuron_init(int length);
+void prop_error_back(neuron *hidden, neuron output);
+float finalError (int *input, int lines, neuron output);
+void weight_ajust (neuron *hidden, neuron output);
+void freadP(char *str, int length, FILE *file);
+void fwriteP(neuron *hidden, neuron output, FILE *file);
+float parseWeight(int i, char *weight);  
+void loadWeight(neuron *hidden, neuron output, char *weight);
+
+#endif
