7cc3465adc70540ab638f0147a0b2ef370d6a288
Raphael Heimann <raphael.heimann@epita.fr>
2017-10-21 12:13:01 +0200

    bla

--- image_op/main.c
+++ image_op/main.c
@@ -55,14 +55,12 @@ SDL_Surface* display_image(SDL_Surface *img)
 }
 
 
-int main(/*int argc, char *argv[]*/)
+int main(int argc, char *argv[])
 {
-  /*
   if(argc != 2)
     printf("Error: wrong number of args");
-  */
   init_sdl();
-  SDL_Surface* image = load_image("Image1.jpg");
+  SDL_Surface* image = load_image(argv[1]);
   display_image(image);
   set_pixel_gray(image);
   display_image(image);
