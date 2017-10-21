360c7ef8cfac87012ec6f534034d8e1f0b6da917
Antoine Hacquard <antoine.hacquard@epita.fr>
2017-10-21 14:49:20 +0200

    rm de fichiers inutile sur la branche master

--- pixel_color.h
+++
@@ -1,8 +0,0 @@
-# ifndef PIXEL_COLOR_H_
-# define PIXEL_COLOR_H_
-
-# include <stdlib.h>
-# include <SDL.h>
-
-Uint32 getpixel(SDL_Surface *surface, unsigned x, unsigned y);
-void putpixel(SDL_Surface *surface, unsigned x, unsigned y, Uint32 pixel);
