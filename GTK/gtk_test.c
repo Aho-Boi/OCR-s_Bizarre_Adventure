259d64352579e356b84e1fbfc113bcc701ecd243
Ugo Chane You Kaye <ugochane@gmail.com>
2017-11-20 01:29:31 +0100

    Add du dossier GTK avec Makefile inclu

---
+++ GTK/gtk_test.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <gtk.h>
+
+int main(int argc, char **argv)
+{
+  GtkWidget * MainWindow = NULL;
+  MainWindow = gtk_new_window(GTK_WINDOW_TOPLEVEL);
+  gtk_init(&argc, &argv);
+  gtk_widget_show(MainWindow);
+  return EXIT_SUCCESS;
+}
