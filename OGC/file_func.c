1d8c4438b874ef9673a5ce9d8b10bb0ae57b946a
Antoine Hacquard <antoine.hacquard@epita.fr>
2017-12-05 23:46:26 +0100

    se

--- OGC/file_func.c
+++ OGC/file_func.c
@@ -9,7 +9,7 @@ void mat_write(matrix_t *mat, FILE *file)
   for(int i = 0; i < mat->height; ++i)
   {
     for(int j = 0; j < mat->witdh; ++j)
-      fprintf(file, " %g", mat->mat[i * mat->width + j]);
+      fprintf(file, " %8g", mat->mat[i * mat->width + j]);
     fprintf(file, "\n");
   }
   fprintf(file, "LOLI!!!\n");
@@ -24,15 +24,39 @@ void write(matrix_t *neuron, matrix_t *output, char *cfile)
 }
 
 static
-void mat_read(matrix_t *mat, FILE *file)
+char* read_word(int* i, char* content, size_t size)
 {
+  char *res = calloc(sizeof(char), 9);
 
+  for(int j = 0; i < size && content[i] != ' ' && content[i] != '\n'; ++i, ++j)
+    res[j] += content[i];
+  i++;
+
+  return res;
+}
+
+static
+void mat_read(int* i, char* content, size_t size, matrix_t *mat)
+{
+  char *word = read_word(&i, content, size);
+  double m = mat->mat;
+  do 
+  {
+    *m = atof(word);
+    m++;
+    word = read_word(&i, content, size);
+  } while(word != "LOLI!!!");
 }
 
 void read(matrix_t *neuron, matrix_t *output, char *cfile)
 {
   FILE *file = fopen(cfile, "r");
-  mat_write(neuron, file);
-  mat_write(output, file);
+  size_t size = fsize(file);
+  char *content = malloc(sizeof(char) * size);
+  fread(content, sizeof(char), size, file);
   fclose(file;)
+
+  int i = 0;
+  mat_read(&i, content, size, neuron);
+  mat_read(&i, cotnent, size, output);
 }
